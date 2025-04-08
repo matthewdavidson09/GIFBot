7686bae2d88b8cf7a66c9a46296fcca33fdcb20d
Matt Davidson <github@idolthought.com>
2025-04-07 20:22:23 -0700

    update to token, from bearer

--- github.go
+++ github.go
@@ -14,7 +14,7 @@ func PostComment(token, repo string, issueNumber int, body string) error {
 	jsonPayload, _ := json.Marshal(payload)
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Authorization", "token "+token)
 	req.Header.Set("Accept", "application/vnd.github.v3+json")
 
 	resp, err := http.DefaultClient.Do(req)
